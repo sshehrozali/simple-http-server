79dbe441f946b3dd9396e7cc8d4d799a74988b24
sshehrozali <sshehroza13@gmail.com>
2024-09-06 15:41:19 +0500

    Wrapped http handler in a goroutine

--- server.go
+++ server.go
@@ -11,14 +11,21 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	http.HandleFunc("/", getHandler)
+	// Don't block the main thread, start HTTP server in a new goroutine
+	go func() {
+		http.HandleFunc("/", getHandler)
 
-	fmt.Println("Starting server on port 8080...")
+		fmt.Println("Starting server on port 8080...")
+	
+		err:= http.ListenAndServe(":8080", nil)
+		if (err != nil) {
+			fmt.Println("Error starting server on port 8080...")
+		}
+	}()
 
-	err:= http.ListenAndServe(":8080", nil)
-	if (err != nil) {
-		fmt.Println("Error starting server on port 8080...")
-	}
+	println("Server is running in the background..and you can perform tasks asynchronously")
 
-	fmt.Println("Hello Golang!")
+	// Keep running the main thread (blocks it forever)
+	select{}
+
 }
